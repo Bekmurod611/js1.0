18efe17b94b0a15f5f93ed8a7f7068ed6083d408
--global <--global>
2021-04-11 12:52:35 +0500

    versiya 1.1

---
+++ script.js
@@ -0,0 +1,18 @@
+var javob = prompt('Ismingiz kiriting?')
+console.log (('Mening ismim ') + javob)
+
+var javob = prompt('Yoshingiz kiriting?')
+console.log (('Men ') + javob + " yoshdaman")
+
+var javob = prompt('Misolni yeching 6+3=?')
+console.log (('Misol 1: 6 + 3 = 9 ') + ' Sizning javob ' + javob)
+var javob = prompt('Misolni yeching 20-5=?')
+console.log (('Misol 2: 20 - 5 = 15 ') + ' Sizning javob ' + javob)
+var javob = prompt('Misolni yeching 15*2=?')
+console.log (('Misol 3: 15 * 2 = 30 ') + ' Sizning javob ' + javob)
+var javob = prompt('Misolni yeching 8/4=?')
+console.log (('Misol 4: 8 / 4 = 2 ') + ' Sizning javob ' + javob)
+var javob = prompt('Misolni yeching 10/2=?')
+console.log (('Misol 5: 10 / 2 = 5 ') + ' Sizning javob ' + javob)
+
+alert ('Natijani ko`rish uchun consoleni bosing')
